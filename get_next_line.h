a11666bd39899962056abccad32000ed71125817
skyrunner176 <nbouchib@e1r9p3.42.fr>
2014-12-30 13:30:07 +0100

    42 project : Get Next Line

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbouchib <nbouchib@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/11/23 14:23:22 by nbouchib          #+#    #+#             */
+/*   Updated: 2014/12/03 02:15:47 by nbouchib         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 8
+
+int		get_next_line(int fd, char **line);
+
+#endif
